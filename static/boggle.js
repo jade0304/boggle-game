d54f1c0c6cccfd1451495832a66ac3d7c7d69eca
jade0304 <stefanie.dn88@gmail.com>
2021-11-22 11:12:33 -0600

    fixing bugs

--- static/boggle.js
+++ static/boggle.js
@@ -1,90 +1,98 @@
 class BoggleGame {
-
+  
     constructor(boardId, secs = 60) {
-        this.secs = secs; // game length
-        this.showTimer();
-
-        this.score = 0
-        this.words = new Set(); 
-        this.board = $("#" + boardId)
-
-        this.timer = setInterval(this.countDown.bind(this), 1000);
-
-        $('.add-word', this.board).on('submit', this.handleSubmit.bind(this));
+      this.secs = secs; // game length
+      this.showTimer();
+  
+      this.score = 0;
+      this.words = new Set();
+      this.board = $("#" + boardId);
+  
+      this.timer = setInterval(this.tick.bind(this), 1000);
+  
+      $(".add-word", this.board).on("submit", this.handleSubmit.bind(this));
     }
-    
-
-    showListOfWords(word){
-        $('.words', this.board).append($("<li>", {text: word}))
+  
+    /* show word in list of words */
+  
+    showWord(word) {
+      $(".words", this.board).append($("<li>", { text: word }));
     }
-
-    score(){
-        $('.score', this.board).text(this.score)
+  
+    /* show score in html */
+  
+    showScore() {
+      $(".score", this.board).text(this.score);
     }
-
-    message(msg, cls){
-        $('.msg', this.board)
+  
+    /* show a status message */
+  
+    showMessage(msg, cls) {
+      $(".msg", this.board)
         .text(msg)
         .removeClass()
         .addClass(`msg ${cls}`);
     }
-
-
+  
+    /* handle submission of word: if unique and valid, score & show */
+  
     async handleSubmit(evt) {
-        evt.preventDefault();
-        const $word = $('.word', this.board)
-
-        let word = $word.val();
-        if (!word) return
-
-        if (this.words.has(word)) {
-            this.message(`Already found ${word}`, 'error')
-            return;
-        }
-
-    const resp = await axios.get('/check-word', {params: {word: word}});
-    if (resp.data.result === "not-word") {
-        this.message(`${word} is not a valid english word`, 'error')
-    } else if (resp.data.result === "not-on-board") {
-        this.message(`${word} is not a valid word on this board`, 'error')
-    } else {
-        this.showListOfWords(word);
-        this.score += word.length;
-        this.score();
-        this.words.add(word);
-        this.message(`Added: ${word}`, 'ok');
-    }
-console.log(this.message(`Added: ${word}`, 'ok'))
-    $word.val("").focus();
-}
-
-
-showTimer(){ 
-    let $timer = $(".timer", this.board)
-    $timer.text(this.secs);
-    console.log($timer)
-    console.log(this.secs)
-}
-
-async countDown(){
-    console.log(this.secs)
-    this.secs -= 1;
-    this.showTimer();
-    
-    if(this.secs === 0){
-    clearInterval(this.timer);
-    await this.gameScore(); 
-}
-}
-
-    async gameScore() {
-        $(".add-word", this.board).hide();
-        const resp = await axios.post("/post-score", { score: this.score });
-        if (resp.data.brokeRecord) {
-          this.message(`New record: ${this.score}`, "ok");
-        } else {
-          this.message(`Final score: ${this.score}`, "ok");
-        }
+      evt.preventDefault();
+      const $word = $(".word", this.board);
+  
+      let word = $word.val();
+      if (!word) return;
+  
+      if (this.words.has(word)) {
+        this.showMessage(`Already found ${word}`, "err");
+        return;
       }
-}
-
+  
+      // check server for validity
+      const resp = await axios.get("/check-word", { params: { word: word }});
+      if (resp.data.result === "not-word") {
+        this.showMessage(`${word} is not a valid English word`, "err");
+      } else if (resp.data.result === "not-on-board") {
+        this.showMessage(`${word} is not a valid word on this board`, "err");
+      } else {
+        this.showWord(word);
+        this.score += word.length;
+        this.showScore();
+        this.words.add(word);
+        this.showMessage(`Added: ${word}`, "ok");
+      }
+  
+      $word.val("").focus();
+    }
+  
+    /* Update timer in DOM */
+  
+    showTimer() {
+      $(".timer", this.board).text(this.secs);
+    }
+  
+    /* Tick: handle a second passing in game */
+  
+    async tick() {
+      this.secs -= 1;
+      this.showTimer();
+  
+      if (this.secs === 0) {
+        clearInterval(this.timer);
+        await this.scoreGame();
+      }
+    }
+  
+    /* end of game: score and update message. */
+  
+    async scoreGame() {
+      $(".add-word", this.board).hide();
+      const resp = await axios.post("/post-score", { score: this.score });
+      if (resp.data.brokeRecord) {
+        this.showMessage(`New record: ${this.score}`, "ok");
+      } else {
+        this.showMessage(`Final score: ${this.score}`, "ok");
+      }
+    }
+  }
+  
